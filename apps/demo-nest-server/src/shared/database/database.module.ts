685fab5e3f96babbc2f54840be44710747981867
droguljic <1875821+droguljic@users.noreply.github.com>
2025-07-08 17:06:16 +0200

    build: update dependencies (#2)
    Push all dependencies to the latest version and
    make changes to the codebase to address breaking changes.

--- apps/demo-nest-server/src/shared/database/database.module.ts
+++ apps/demo-nest-server/src/shared/database/database.module.ts
@@ -1,4 +1,5 @@
 import { MikroOrmModule, MikroOrmModuleOptions } from '@mikro-orm/nestjs';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
@@ -11,6 +12,7 @@ export class DatabaseModule {
         MikroOrmModule.forRootAsync({
           imports: [ConfigModule],
           inject: [ConfigService],
+          driver: PostgreSqlDriver,
           useFactory: (config: ConfigService) => ({
             ...config.get('database'),
             autoLoadEntities: true,
